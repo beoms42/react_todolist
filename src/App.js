1d574c2e678e573460222fb8a66182079de4b2fe
yicho <yicho@spectra.co.kr>
2023-08-22 17:58:11 +0900

    p316 08/22

--- src/App.js
+++ src/App.js
@@ -1,13 +1,52 @@
 import './App.css';
 import Header from "./component/Header"
+import TodoEditor from "./component/TodoEditor";
+import TodoList from "./component/TodoList";
+import {useState} from "react";
 
-    /***
-     * P298 (좌측 하단기준)
+
+/***
+     * P316 (좌측 하단기준)
      */
+
+const mockTodo = [
+    {
+        id: 0,
+        isDone: false,
+        content: "React 공부하기",
+        createDate: new Date().getTime(),
+    },
+    {
+        id: 1,
+        isDone: false,
+        content: "놀기",
+        createDate: new Date().getTime(),
+    },
+    {
+        id: 2,
+        isDone: false,
+        content: "밥먹기",
+        createDate: new Date().getTime(),
+    },
+];
 function App() {
+    const [todo, setTodo] = useState(mockTodo);
+
+
+    const onCreate = (content) => {
+        const newItem = {
+            id: 0,
+            content,
+            isDone: false,
+            createDate: new Date().getTime(),
+        };
+        setTodo([newItem, ...todo]);
+    };
   return (
     <div className="App">
         <Header />
+        <TodoEditor/>
+        <TodoList/>
         <h2>Hello React.1</h2>
     </div>
   );
